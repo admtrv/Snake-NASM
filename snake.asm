67d70b8d0834c8fed1f6f501dfea7c1ceb23fea6
Anton Dmitriev <tosha.dmitriefff@yandex.ru>
2025-08-22 23:03:01 +0200

    style: remove unnecessary comments

--- snake.asm
+++ snake.asm
@@ -8,22 +8,19 @@
 %define STDIN   0
 %define STDOUT  1
 
-; termios codes to read and write
-%define TCGETS  0x5401
-%define TCSETS  0x5402
+; termios codes
+%define TCGETS  0x5401  ; read
+%define TCSETS  0x5402  ; write
 
 ; termios flags and layout
-%define ICANON  0x00000002   
-%define ECHO    0x00000008
-%define ISIG    0x00000001
-%define IXON    0x00000400
+%define ICANON  0x00000002  ; canonical mode - wait Enter
+%define ECHO    0x00000008  ; echo mode - duplicate input
+%define ISIG    0x00000001  ; signals - Ctrl+C, Ctrl+Z 
+%define IXON    0x00000400  ; software flow control - Ctrl+S, Ctrl+Q
 
-%define OFF_IFLAG   0
-%define OFF_OFLAG   4
-%define OFF_CFLAG   8
-%define OFF_LFLAG   12
-%define OFF_LINE    16
-%define OFF_CC      17
+%define OFF_IFLAG   0   ; c_iflag (input flags)
+%define OFF_LFLAG   12  ; c_lflag (local flags)
+%define OFF_CC      17  ; c_cc[] (control chars)
 
 %define TERMIOS_SZ 60
 
@@ -33,8 +30,10 @@
 section .data
 
 section .bss
-    t_old resb TERMIOS_SZ   ; buffer for old termios settings
-    t_new resb TERMIOS_SZ   ; buffer for new termios settings
+    ; buffer for termios settings
+    t_old resb TERMIOS_SZ   ; old
+    t_new resb TERMIOS_SZ   ; new
+
     key resb 1
 
 section .text
@@ -49,24 +48,6 @@ _start:
     syscall
 
     ; copy t_old -> t_new
-    ;
-    ; copy algorithm:
-    ;
-    ;   rdi - pointer `to`
-    ;   rsi - pointer `from`
-    ;
-    ;   cld (clear direction flag):
-    ;       df (direction flag) in register rflags
-    ;           if df = 0 -> rsi and rdi increase
-    ;           if df = 1 -> rsi and rdi decrease
-    ;
-    ;   movsb (move string byte) - copy one byte:
-    ;       [rdi] = [rsi]
-    ;       rsi += 1 (if df = 0)
-    ;       rdi += 1
-    ;
-    ;   rep - repeat command `rcx` times
-    ;       => rcx - size
     lea rsi, [rel t_old]
     lea rdi, [rel t_new]
     mov rcx, TERMIOS_SZ     
@@ -74,19 +55,18 @@ _start:
     rep movsb
 
     ; raw mode
-    mov eax, [rel t_new + OFF_LFLAG]    ; c_lflag (local flags)
-    and eax, ~ICANON                    ; canonical mode - wait Enter
-    and eax, ~ECHO                      ; echo mode - duplicate input
-    and eax, ~ISIG                      ; signals - Ctrl+C, Ctrl+Z 
+    mov eax, [rel t_new + OFF_LFLAG]
+    and eax, ~(ICANON | ECHO | ISIG)
+    and eax, ~ECHO
+    and eax, ~ISIG
     mov [t_new + OFF_LFLAG], eax
     
-    mov eax, [rel t_new + OFF_IFLAG]    ; c_iflag (input flags)
-    and eax, ~IXON                      ; software flow control - Ctrl+S, Ctrl+Q
+    mov eax, [rel t_new + OFF_IFLAG]
+    and eax, ~IXON
     mov [t_new + OFF_IFLAG], eax
 
-    ; c_cc[] (control chars)
-    mov byte [t_new + OFF_CC + VMIN], 0     ; not require bytes for read
-    mov byte [t_new + OFF_CC + VTIME], 1    ; wait maximum 0.1 seconds
+    mov byte [t_new + OFF_CC + VMIN], 0
+    mov byte [t_new + OFF_CC + VTIME], 1
 
     ; apply new termios
     mov rax, SYS_IOCTL
@@ -131,7 +111,7 @@ _start:
     lea rdx, [rel t_old]
     syscall
 
-    ; exit(status)
+    ; exit
     mov rax, SYS_EXIT
-    xor rdi, rdi        ; make zero byte
+    xor rdi, rdi
     syscall
